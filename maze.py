476c7af5946e5cba459d8ef76f6e85b2ee6cad1d
Tim Golden <mail@timgolden.me.uk>
2013-05-02 20:28:24 +0100

    Add skeleton maze

---
+++ maze.py
@@ -0,0 +1,25 @@
+import os, sys
+import random
+
+class Path(object):
+
+    def __init__(self):
+        self.start = None
+        self.stop = None
+        self.steps = []
+
+class Maze(object):
+
+    w, h = 400, 400
+
+    def __init__(self):
+        self.paths = []
+
+    def generate_path(self, start, stop):
+        pass
+
+    def generate_paths(self, n_paths=10):
+        for i in range(n_paths):
+            start = random()
+            stop = random()
+            self.paths.append(self.generate_path(start, stop))
