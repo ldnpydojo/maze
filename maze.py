083668e8a32f75b2d925f6138e7c7dce31ddd3de
Tim Golden <mail@timgolden.me.uk>
2013-05-02 21:05:55 +0100

    Remove cruft from previous attempts

--- maze.py
+++ maze.py
@@ -5,13 +5,6 @@ from collections import namedtuple
 
 Segment = namedtuple("Segment", "angle distance")
 
-class Path(object):
-
-    def __init__(self, start, stop):
-        self.start = start
-        self.stop = stop
-        self.steps = []
-
 class Maze(object):
 
     w, h = 400, 400
@@ -28,17 +21,6 @@ class Maze(object):
         self.generate_paths(x, w / 2, n - 1, result)
         result.append(Segment(angle, w))
         result.append(Segment(-2 * angle, w))
-
-    def generate_path_1(self, start, stop):
-        waypoints = []
-        waypoints.append(start)
-        waypoints.append(stop)
-
-    def generate_paths_2(self, n_paths=10):
-        for i in range(n_paths):
-            start = 0, random.randint(self.h)
-            stop = self.w, random.randint(self.h)
-            self.paths.append(self.generate_path(start, stop))
 
 
 m = Maze()
