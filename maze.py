805b542e4bd67e0a40746c024144c01223f4059d
Tim Golden <mail@timgolden.me.uk>
2013-05-02 20:50:45 +0100

    Generate waypoints

--- maze.py
+++ maze.py
@@ -17,12 +17,21 @@ class Maze(object):
 
     def __init__(self):
         self.paths = []
+        self.entry = point_1
+        self.exit = point_2
 
     def generate_path(self, start, stop):
-        pass
+        waypoints = []
+        waypoints.append(start)
+        for x in range(10):
+            waypoints.append(
+                (random.randint(self.w),
+                random.randint(self.h))
+            )
+        waypoints.append(stop)
 
     def generate_paths(self, n_paths=10):
         for i in range(n_paths):
-            start = random()
-            stop = random()
+            start = 0, random.randint(self.h)
+            stop = self.w, random.randint(self.h)
             self.paths.append(self.generate_path(start, stop))
